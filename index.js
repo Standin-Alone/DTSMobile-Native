ac62ecebf131b22bfbea67cfde1a84bb52e8118e
standinalone01000 <johnedcelzenarosa@gmail.com>
2022-01-19 18:54:31 +0800

    push origin master

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import {AppRegistry} from 'react-native';
 // import App from './App';
 import Route from './navigation/Route';
 import {name as appName} from './app.json';
-
+import 'react-native-gesture-handler';
 
 
 
--- ipconfig.tsx
+++ ipconfig.tsx
@@ -4,4 +4,4 @@
 export  const ipAddress = 'https://devsysadd.da.gov.ph/dts/'; //https:/172.16.200.41/dts/ //DEV SERVER IP
 //export  const ipAddress = 'http://172.17.150.112/dts-web/'; //IP SA OFFICE (ICT)
 
-// export  const ipAddress = 'http://192.168.56.1/dts-web/';
+ //export  const ipAddress = 'http://192.168.1.7/dts-web/';
